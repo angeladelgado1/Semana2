4120e9b9aa11d36338eb535a956360941add0eb5
ajdm05 <angela.delgado@unitec.edu>
2013-04-22 23:56:49 -0600

    Update Player.h

--- Player.h
+++ Player.h
@@ -1,9 +1,9 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
-#include "SDL/SDL.h"
-#include "SDL/SDL_image.h"
-#include "SDL/SDL_ttf.h"
+#include "SDL.h"
+#include "SDL_image.h"
+
 #include <string>
 
 class Player
@@ -14,7 +14,7 @@ public:
     int velocity;
     int acceleration;
     int current_frame;
-    SDL_Surface *images[4];
+    SDL_Surface *images[5];
     SDL_Surface *screen;
 
     Player(SDL_Surface *screen);
